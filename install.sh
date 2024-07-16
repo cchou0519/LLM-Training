cb7ef30fece678d86228f1d290c3bb45e5b327df
ShinoharaHare <anna.shiker1822@gmail.com>
2024-07-16 19:56:48 +0800

    chore: 強制重新建置 flash_attn

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# Make sure `packaging` is installed before installing `flash_attn`
 pip install packaging
+# Force a rebuild of `flash_attn` in case .so files built with an incompatible version of CUDA is cached.
+pip install flash_attn --no-build-isolation --no-cache-dir
+
 pip install -e .[deepspeed]
