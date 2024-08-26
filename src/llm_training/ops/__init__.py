c59e420ee36aba1a6b73345798b7cccc6128bcbf
ShinoharaHare <anna.shiker1822@gmail.com>
2024-08-26 09:47:33 +0800

    fix: 修復 import 錯誤

--- src/llm_training/ops/__init__.py
+++ src/llm_training/ops/__init__.py
@@ -1,4 +1,3 @@
-from .attention_op import get_unpad_data
 from .cross_entropy_op import cross_entropy, shift_labels
 from .rms_norm_op import rms_norm
 from .rope_op import fast_rope_embedding, inplace_rope_embedding
