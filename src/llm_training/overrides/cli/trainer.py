05ca58cf0668fea193ab3caa0094f44f626d8024
ShinoharaHare <anna.shiker1822@gmail.com>
2024-08-31 09:58:56 +0800

    fix: 檢測是否已有 train_dataloader 邏輯錯誤

--- src/llm_training/overrides/cli/trainer.py
+++ src/llm_training/overrides/cli/trainer.py
@@ -4,7 +4,7 @@ from lightning import Trainer as _Trainer
 class Trainer(_Trainer):
     @property
     def estimated_stepping_batches(self) -> int | float:
-        has_train_dataloader = self.train_dataloader is None
+        has_train_dataloader = self.train_dataloader is not None
         r = super().estimated_stepping_batches
         if not has_train_dataloader:
             self.fit_loop._combined_loader = None
