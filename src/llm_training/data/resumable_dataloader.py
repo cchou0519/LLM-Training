1a9d5abed05dbe9c83130ca0502ed864fac583eb
ShinoharaHare <anna.shiker1822@gmail.com>
2024-09-11 09:55:11 +0800

    fix: 改用 `BatchSampler` 及 `batch_idx` 來恢復訓練進度

--- src/llm_training/data/resumable_dataloader.py
+++ src/llm_training/data/resumable_dataloader.py
@@ -1,136 +1,56 @@
-from typing import Any, Iterator, Protocol, Sized
+from typing import Any, Iterable, Iterator
 
-import torch
-import torch.distributed
-from torch.utils.data import (DataLoader, Dataset, DistributedSampler,
-                              RandomSampler, SequentialSampler)
-
-
-class ResumableSequentialSampler(SequentialSampler):
-    def __init__(self, data_source: Sized) -> None:
-        super().__init__(data_source)
-
-        self.used_indices = set()
-
-    def __iter__(self) -> Iterator[int]:
-        for i, index in enumerate(super().__iter__()):
-            is_used = index in self.used_indices
-
-            self.used_indices.add(index)
-
-            if i + 1 == len(self.data_source):
-                self.used_indices.clear()
-            
-            if not is_used:
-                yield index
+from lightning import Trainer
+from torch.utils.data import BatchSampler, DataLoader
+from torch.utils.data.sampler import Sampler
 
 
-class ResumableRandomSampler(RandomSampler):
+class ResumableBatchSampler(BatchSampler):
     def __init__(
         self,
-        data_source: Sized,
-        replacement: bool = False,
-        num_samples: int | None = None,
-        generator: torch.Generator | None = None
+        sampler: Sampler[int] | Iterable[int],
+        batch_size: int,
+        drop_last: bool,
+        trainer: Trainer | None = None
     ) -> None:
-        generator = generator or torch.Generator().manual_seed(0)
-
-        super().__init__(data_source, replacement, num_samples, generator)
-
-        self.used_indices = set()
-
-    def __iter__(self) -> Iterator[int]:
-        for i, index in enumerate(super().__iter__()):
-            is_used = index in self.used_indices
-
-            self.used_indices.add(index)
-
-            if i + 1 == self.num_samples:
-                self.used_indices.clear()
-            
-            if not is_used:
-                yield index
-
-
-class ResumableDistributedSampler(DistributedSampler):
-    def __init__(
-        self,
-        dataset: Dataset,
-        num_replicas: int | None = None,
-        rank: int | None = None,
-        shuffle: bool = True,
-        seed: int = 0,
-        drop_last: bool = False,
-    ) -> None:
-        super().__init__(dataset, num_replicas, rank, shuffle, seed, drop_last)
+        super().__init__(sampler, batch_size, drop_last)
         
-        self.used_indices = set()
+        self.trainer = trainer
 
-    def __iter__(self) -> Iterator[int]:
-        for i, index in enumerate(super().__iter__()):
-            is_used = index in self.used_indices
-            is_padding = i >= (len(self.dataset) // self.num_replicas)
+    def __iter__(self) -> Iterator[list[int]]:
+        for i, indices in enumerate(super().__iter__()):
+            if self.trainer is not None and i < self.trainer.fit_loop.batch_idx:
+                continue
 
-            if not is_padding:
-                self.used_indices.add(index)
-
-            if i + 1 == self.num_samples:
-                self.used_indices.clear()
-            
-            if not is_used or is_padding:
-                yield index
-
-
-class ResumableSamplerProtocol(Protocol):
-    used_indices: set[int]
+            yield indices
 
 
 class ResumableDataLoader(DataLoader):
-    sampler: ResumableSamplerProtocol
-
     def __init__(
         self,
-        dataset: Dataset,
-        shuffle: bool = True,
-        sampler: None = None,
-        num_replicas: int | None = None,
-        rank: int | None = None,
-        generator: torch.Generator | None = None,
+        trainer: Trainer | None = None,
         *args,
         **kwargs
     ):
-        assert sampler is None, f"To use {self.__class__.__name__}, `sampler` must be None."
+        self.trainer = trainer
 
-        if num_replicas is not None and rank is not None:
-            kwargs['sampler'] = ResumableDistributedSampler(
-                dataset,
-                num_replicas=num_replicas,
-                rank=rank,
-                shuffle=shuffle
+        super().__init__(*args, **kwargs)
+
+    def __setattr__(self, attr: str, val: Any):
+        if attr == 'batch_sampler':
+            assert isinstance(val, BatchSampler)
+
+            val = ResumableBatchSampler(
+                sampler=val.sampler,
+                batch_size=val.batch_size,
+                drop_last=val.drop_last,
+                trainer=self.trainer
             )
-        elif shuffle:
-            kwargs['sampler'] = ResumableRandomSampler(dataset, generator=generator)
-        else:
-            kwargs['sampler'] = ResumableSequentialSampler(dataset)
-
-        super().__init__(dataset, *args, **kwargs)
-
-    def sync_state(self) -> None:
-        if not torch.distributed.is_initialized():
-            return
         
-        # sync used_indices
-        l = [None] * torch.distributed.get_world_size()
-        torch.distributed.all_gather_object(l, self.sampler.used_indices)
-        self.sampler.used_indices.clear()
-        self.sampler.used_indices.update(y for x in l for y in x)
+        return super().__setattr__(attr, val)
 
     def state_dict(self) -> dict[str, Any]:
-        self.sync_state()
-
-        return {
-            'sampler.used_indices': self.sampler.used_indices
-        }
+        return {}
 
     def load_state_dict(self, state_dict: dict[str, Any]) -> None:
-        self.sampler.used_indices = state_dict['sampler.used_indices']
+        ...
