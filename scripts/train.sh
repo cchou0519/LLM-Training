5668b3b1cb6438c6154f545796b29a47f5800603
ShinoharaHare <anna.shiker1822@gmail.com>
2024-12-05 04:03:05 +0800

    chore: 更新 SLURM 訓練腳本

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,18 +1,18 @@
-JOB_NAME=
-NODES=
-PARTITION=
-ACCOUNT=
-GPUS_PER_NODE=
-CPUS_PER_TASK=
-EXTRA_ARGS=(
-    --no-requeue
-)
-CONFIG=
-CKPT_PATH=null
-
 # export TRITON_PTXAS_PATH=$CONDA_PREFIX/bin/ptxas
 # export TRITON_CUOBJDUMP_PATH=$CONDA_PREFIX/bin/cuobjdump
 # export TRITON_NVDISASM_PATH=$CONDA_PREFIX/bin/nvdisasm
+
+JOB_NAME=
+PARTITION=
+ACCOUNT=
+NODES=
+GPUS_PER_NODE=
+CPUS_PER_TASK=
+EXTRA_ARGS=(
+)
+CONFIG=null
+CKPT_PATH=null
+
 
 COMMAND=(
     srun llm-training fit
@@ -53,11 +53,20 @@ fi
 
 JOB_ID=$(echo $SBATCH_OUTPUT | sed "s/Submitted batch job //")
 
-SATTACH_START_TIME=$SECONDS
-while [ $(($SECONDS - $SATTACH_START_TIME)) -le 30 ]
+echo "Waiting for the job to start"
+while [[ $JOB_STATE != "RUNNING" ]]
+do
+    JOB_STATE=$(squeue -j $JOB_ID -h -o %T)
+    sleep 1
+done
+
+echo "The job is running, trying to attach to the output stream ..."
+sleep 3
+
+
+while [[ $JOB_STATE == "RUNNING" ]]
 do
     SATTACH_OUTPUT=$(sattach $JOB_ID.0 2>&1 | tee /dev/tty)
-
     if [[ $SATTACH_OUTPUT == *"Job/step already completing or completed"* ]] \
     || [[ $SATTACH_OUTPUT == *"Socket timed out on send/recv operation"* ]] \
     || [[ $SATTACH_OUTPUT == *"does not look like a jobid"* ]];
@@ -66,3 +75,10 @@ do
     fi
     sleep 1
 done
+
+# while [[ $JOB_STATE == "RUNNING" ]]
+# do
+#     JOB_STATE=$(squeue -j $JOB_ID -h -o %T)
+#     tail -f slurm-$JOB_ID.out
+#     sleep 1
+# done
