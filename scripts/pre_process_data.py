d8c3782069f1ee64d78d8a296bd9c72dc640e154
ShinoharaHare <anna.shiker1822@gmail.com>
2024-12-10 15:20:22 +0800

    chore: 移除強制開啟快取的機制

--- scripts/pre_process_data.py
+++ scripts/pre_process_data.py
@@ -31,21 +31,17 @@ def main():
     
     config = datamodule.config
 
-    enable_cache = config.enable_cache
     pre_processed_data_path = config.pre_processed_data_path
+
+    assert pre_processed_data_path is not None, "`pre_processed_data_path` should not be `None`."
 
     if not os.path.exists(pre_processed_data_path) or len(os.listdir(pre_processed_data_path)) == 0:
         config.pre_processed_data_path = None
-        config.enable_cache = True
         datamodule.setup()
         datamodule.save_pre_processed_data(pre_processed_data_path)
     else:
         print(f'`pre_processed_data_path="{pre_processed_data_path}"` is not empty, skipping.')
         datamodule.setup()
-
-    if not enable_cache:
-        n =  datamodule.cleanup_cache_files()
-        print(f'Cleanup cache files: {n}')
     
     with open(os.path.join(pre_processed_data_path, 'info.txt'), 'w') as f:
         datamodule.print_dataset_info(file=OutputStreamRedirector(sys.stdout, f))
